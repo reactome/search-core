0ef3ab502bc40edb936fa75558574eac266c143e
EliotRagueneau <orkeliot@gmail.com>
2021-03-03 18:03:51 +0000

    Use dismax trick for /content/search/query and add a simplified paginated endpoint

--- src/main/java/org/reactome/server/search/service/SearchService.java
+++ src/main/java/org/reactome/server/search/service/SearchService.java
@@ -316,6 +316,7 @@ public class SearchService {
 
     private void setPagingParameters(Query query, FacetMapping facetMapping, int rowCount, int page, boolean cluster) {
         int typeCount;
+        if (query.getStart() != null && query.getRows() != null) return;
         if (query.getTypes() != null && !query.getTypes().isEmpty()) {
             typeCount = query.getTypes().size();
         } else {
